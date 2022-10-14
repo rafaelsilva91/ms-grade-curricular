2606d4ff7f45c28ea70cdf4533c1f596c4f86a52
Rafael Rodrigues <rafaelsilvarodrigues_@hotmail.com>
2022-10-13 23:14:31 -0400

    First commit - Project Created

---
+++ src/main/java/com/rasmoo/cliente/escola/msgradecurricular/MsGradeCurricularApplication.java
@@ -0,0 +1,13 @@
+package com.rasmoo.cliente.escola.msgradecurricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGradeCurricularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGradeCurricularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rasmoo/cliente/escola/msgradecurricular/MsGradeCurricularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasmoo.cliente.escola.msgradecurricular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGradeCurricularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
